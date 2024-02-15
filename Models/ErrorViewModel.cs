6e8fb95dfd7255274376003834879cfbc5afcd06
Hugo071 <99701511+Hugo071@users.noreply.github.com>
2024-02-14 21:20:30 -0600

    Se agrega el proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MadereriaLaCienega.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
